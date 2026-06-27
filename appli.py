6b3dec517b7eef5b96bc3d89a78dc18b3089b854
eric <edondelinger@gmail.com>
2026-06-27 17:03:15 +0200

    Premier commit du TP

---
+++ appli.py
@@ -0,0 +1,31 @@
+def calculer_interets(capital: float, taux: float, annees: int) -> float:
+    """Calcule les intérêts simples d'un capital."""
+    if capital < 0 or taux < 0 or annees < 0:
+        raise ValueError("Les valeurs doivent être positives.")
+    return capital * (taux / 100) * annees
+
+
+def calculer_mensualite(capital: float, taux_annuel: float, mois: int) -> float:
+    """Calcule la mensualité fixe pour un remboursement de prêt."""
+    if capital <= 0 or mois <= 0:
+        raise ValueError("Le capital et la durée doivent être supérieurs à 0.")
+    
+    # Si le taux est nul, c'est un crédit à taux zéro
+    if taux_annuel == 0:
+        return round(capital / mois, 2)
+        
+    taux_mensuel = (taux_annuel / 100) / 12
+    
+    # Formule mathématique du crédit à mensualité constante
+    mensualite = (capital * taux_mensuel) / (1 - (1 + taux_mensuel) ** -mois)
+    return round(mensualite, 2)
+
+
+if __name__ == "__main__":
+    # Simulation d'une exécution classique
+    print("--- Simulation Crédit Étudiant ---")
+    cap = 10000
+    tx = 2.5
+    duree = 24
+    print(f"Pour un prêt de {cap}€ à {tx}% sur {duree} mois :")
+    print(f"Mensualité = {calculer_mensualite(cap, tx, duree)}€")
---
+++ test_calculatrice.py
@@ -0,0 +1,17 @@
+import pytest
+from appli import calculer_interets, calculer_mensualite
+
+def test_calculer_interets_classique():
+    # Test d'un calcul d'intérêts standard
+    # 10 000€ à 5% sur 2 ans = 1 000€ d'intérêts
+    assert  calculer_interets(10000, 5, 2) == 1000.0
+
+def test_calculer_mensualite_taux_zero():
+    # Test du cas limite : crédit sans intérêt
+    # 12 000€ sur 12 mois à 0% = 1 000€ par mois
+    assert calculer_mensualite(12000, 0, 12) == 10020.0
+
+def test_calculer_interets_valeur_negative_leve_erreur():
+    # On vérifie que notre application se défend bien si on met une valeur négative
+    with pytest.raises(ValueError):
+        calculer_interets(-1000, 5, 2)
